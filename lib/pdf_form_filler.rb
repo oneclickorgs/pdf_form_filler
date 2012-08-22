a49223b52db1f20e67efb44df450d28d1a096088
Chris Mear <chris@feedmechocolate.com>
2012-08-22 14:43:28 +0100

    First pass of a YAML-based form definition language, and a function for highlighting defined fields.

--- lib/pdf_form_filler.rb
+++ lib/pdf_form_filler.rb
@@ -1,69 +1,72 @@
 require 'pdf_form_filler/version'
-require 'prawn'
+require 'pdf_form_filler/form'
 
-axes = false
+module PdfFormFiller
+  def stroke_axis(pdf, options={})
+    options = { :height => (pdf.cursor - 20).to_i,
+                :width => pdf.bounds.width.to_i
+              }.merge(options)
 
-def stroke_axis(pdf, options={})
-  options = { :height => (pdf.cursor - 20).to_i,
-              :width => pdf.bounds.width.to_i
-            }.merge(options)
+    pdf.dash(1, :space => 4)
+    pdf.stroke_horizontal_line(-21, options[:width], :at => 0)
+    pdf.stroke_vertical_line(-21, options[:height], :at => 0)
+    pdf.undash
 
-  pdf.dash(1, :space => 4)
-  pdf.stroke_horizontal_line(-21, options[:width], :at => 0)
-  pdf.stroke_vertical_line(-21, options[:height], :at => 0)
-  pdf.undash
+    pdf.fill_circle [0, 0], 1
 
-  pdf.fill_circle [0, 0], 1
+    (100..options[:width]).step(100) do |point|
+      pdf.fill_circle [point, 0], 1
+      pdf.draw_text point, :at => [point-5, -10], :size => 7
+    end
 
-  (100..options[:width]).step(100) do |point|
-    pdf.fill_circle [point, 0], 1
-    pdf.draw_text point, :at => [point-5, -10], :size => 7
-  end
-
-  (100..options[:height]).step(100) do |point|
-    pdf.fill_circle [0, point], 1
-    pdf.draw_text point, :at => [-17, point-2], :size => 7
+    (100..options[:height]).step(100) do |point|
+      pdf.fill_circle [0, point], 1
+      pdf.draw_text point, :at => [-17, point-2], :size => 7
+    end
   end
 end
 
-input_file = "ms_application_form.pdf"
+# axes = false
 
-pdf = Prawn::Document.new(:template => input_file)
 
-pdf.go_to_page(1)
-stroke_axis(pdf) if axes
+# input_file = "ms_application_form.pdf"
 
-pdf.text_box("OCO Digital IPS", :at => [45, 603])
+# pdf = Prawn::Document.new(:template => input_file)
 
-pdf.go_to_page(2)
-stroke_axis(pdf) if axes
+# pdf.go_to_page(1)
+# stroke_axis(pdf) if axes
 
-pdf.go_to_page(3)
-stroke_axis(pdf) if axes
+# pdf.text_box("OCO Digital IPS", :at => [45, 603])
 
-pdf.text_box("x", :at => [136.5, 613])
-pdf.text_box("x", :at => [403.5, 613])
+# pdf.go_to_page(2)
+# stroke_axis(pdf) if axes
 
-pdf.font_size(9)
-pdf.text_box("Christopher Mear", :at => [230, 365])
-pdf.text_box("Director", :at => [230, 343])
+# pdf.go_to_page(3)
+# stroke_axis(pdf) if axes
 
-pdf.go_to_page(4)
-stroke_axis(pdf) if axes
+# pdf.text_box("x", :at => [136.5, 613])
+# pdf.text_box("x", :at => [403.5, 613])
 
-pdf.go_to_page(5)
-stroke_axis(pdf) if axes
+# pdf.font_size(9)
+# pdf.text_box("Christopher Mear", :at => [230, 365])
+# pdf.text_box("Director", :at => [230, 343])
 
-pdf.go_to_page(6)
-stroke_axis(pdf) if axes
+# pdf.go_to_page(4)
+# stroke_axis(pdf) if axes
 
-pdf.go_to_page(7)
-stroke_axis(pdf) if axes
+# pdf.go_to_page(5)
+# stroke_axis(pdf) if axes
 
-pdf.go_to_page(8)
-stroke_axis(pdf) if axes
+# pdf.go_to_page(6)
+# stroke_axis(pdf) if axes
 
-pdf.go_to_page(9)
-stroke_axis(pdf) if axes
+# pdf.go_to_page(7)
+# stroke_axis(pdf) if axes
 
-pdf.render_file "output.pdf"
+# pdf.go_to_page(8)
+# stroke_axis(pdf) if axes
+
+# pdf.go_to_page(9)
+# stroke_axis(pdf) if axes
+
+# pdf.render_file "output.pdf"
---
+++ lib/pdf_form_filler/form.rb
@@ -0,0 +1,71 @@
+require 'prawn'
+require 'yaml'
+
+class PdfFormFiller::Form
+  def initialize(options={})
+    @template = options[:template]
+
+    @definition_file = options[:definition]
+    @definition = YAML.load_file(@definition_file)
+
+    @pdf = Prawn::Document.new(:template => @template)
+
+    # x_offset, y_offset is the position of the Prawn origin, as expressed
+    # in the co-ordinate system used for the rest of the measurements.
+    #
+    # In Prawn, x runs left to right, and y runs bottom to top. If either
+    # of the axes in your co-ordinate system run opposite to this, set
+    # invert_x and/or invert_y to true.
+  end
+
+  def fill_form(data)
+    1.upto(@pdf.page_count) do |page_number|
+      @pdf.go_to_page(page_number)
+    end
+  end
+
+  def highlight_fields
+    1.upto(@pdf.page_count) do |page_number|
+      @pdf.go_to_page(page_number)
+
+      @pdf.fill_color = "ff0000"
+
+      if @definition[page_number] && @definition[page_number].respond_to?(:each)
+        @definition[page_number].each do |name, box_definition|
+          if box_definition.is_a?(Hash)
+            box_coords = box_definition['box']
+          else
+            box_coords = box_definition
+          end
+          local_box_coords = convert_to_local(box_coords)
+          @pdf.fill_rectangle([local_box_coords[0], local_box_coords[1]], local_box_coords[2], local_box_coords[3])
+        end
+      end
+    end
+  end
+
+  def render_file(output)
+    @pdf.render_file(output)
+  end
+
+protected
+
+  def convert_to_local(box_coords)
+    x_offset = @definition['defaults']['x_offset'] || 0
+    y_offset = @definition['defaults']['y_offset'] || 0
+
+    x = if @definition['defaults']['invert_x']
+      x_offset - box_coords[0]
+    else
+      box_coords[0] - x_offset
+    end
+
+    y = if @definition['defaults']['invert_y']
+      y_offset - box_coords[1]
+    else
+      box_coords[1] - y_offset
+    end
+
+    [x, y, box_coords[2], box_coords[3]]
+  end
+end
